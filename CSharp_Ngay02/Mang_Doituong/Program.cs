58bf214417b1d8c9108b23a8f25e3bccb514c430
Hien Tran <109644638+hienttm@users.noreply.github.com>
2023-04-12 17:43:50 +0700

    up

---
+++ CSharp_Ngay02/Mang_Doituong/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mang_Doituong
+{
+    internal class Program
+    {
+        static Person[] list = new Person[3];
+        static int Timkiem(string key)
+        {
+            for (int i = 0; i < list.Length; i++)
+            {
+                //if(list[i].Hoten.Equals(key))
+                if (list[i].Hoten.CompareTo(key) == 0)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            //Person person = new Person();
+            //Console.WriteLine("Nhập Person:");
+            //person.Nhap();
+            //person.Hienthi();
+            //khai báo và sử dụng mảng kiểu truyền thống
+            int[] iarr = new int[10];//khi khởi tạo mảng là sử dụng được ngay
+            
+            //khai báo và khởi tạo mảng đối tượng
+            //Person[] list = new Person[3];
+            //khai báo và khởi tạo mảng 3 Person, nhưng mỗi phần tử là NULL
+
+            //trước khi làm việc với mỗi phần tử của mảng đối tượng thì 
+            //phải khởi tạo từng đối tượng Person cho mỗi phần tử của mảng
+            //list[0] = new Person();//nếu không khởi tạo thì list[0] == NULL
+            //Console.WriteLine("Nhập phần tử Person 0:");
+            //list[0].Nhap();
+            //list[0].Hienthi();
+            Console.WriteLine("nhập mảng đối tượng bằng vòng lặp");
+            for (int i = 0; i < list.Length; i++)
+            {
+                Console.WriteLine("Nhập phần tử Person {0}:",i+1);
+                list[i] = new Person();
+                list[i].Nhap();
+            }
+            Console.WriteLine("Hiển thị mảng");
+            foreach(Person p in list)
+            {
+                p.Hienthi();
+            }
+            Console.Write("Nhap ten can tim:");
+            string key = Console.ReadLine();
+            int vitri = Timkiem(key);
+            
+            if (vitri >= 0)
+                Console.WriteLine("Tìm thấy:" + list[vitri].Hoten);
+            else
+                Console.WriteLine("Không tìm thấy");
+        }
+    }
+}
---
+++ CSharp_Ngay02/ThamChieu_ThamTri/Vidu_Thamchieu_Thamtri/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vidu_Thamchieu_Thamtri
+{
+    internal class Program
+    {
+        //sử dụng truyền đối số qua tham trị
+        static void thamtri_tang(int x1)
+        {
+            x1++;//tăng tham số x1 lên 1 đơn vị
+            Console.WriteLine("thamtri_tang: x1=" + x1);
+        }
+        //sử dụng truyền đối số qua tham chiếu
+        static void thamchieu_tang(ref int x2)
+        {
+            x2++;//tăng tham số x2 lên 1 đơn vị
+            Console.WriteLine("thamchieu_tang: x2=" + x2);
+        }
+        static void Main(string[] args)
+        {
+            int a = 10;
+            thamtri_tang(a);//sao chép giá trị a cho tham số x1: ô nhớ của x1 độc lập với ô nhớ a 
+            //sau khi kết thúc hàm thamtri_tang, giá a không thay đổi
+            Console.WriteLine("a sau khi tang (tham tri):" + a);
+            int b = 10;
+            thamchieu_tang(ref b);//cho tham số x2 tham chiếu đến ô nhớ b: x2 và b cùng 1 ô nhớ
+            //sau khi kết thúc thamchieu_tang, giá trị x2 thay đổi chính là b
+            Console.WriteLine("b sau khi tang (tham chieu):" + b);
+        }
+    }
+}
