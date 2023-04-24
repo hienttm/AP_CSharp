f1265cafe4c07fb5a556c48b4c68a830a64513c1
Hien Tran <109644638+hienttm@users.noreply.github.com>
2023-04-24 19:06:06 +0700

    up code

---
+++ Baithithu_Ngay07/baithithu_exam_ngay07/baithithu_exam_ngay07/Cylinder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace baithithu_exam_ngay07
+{
+	public class Cylinder
+	{
+		public double Radius;
+		public double Height;
+
+        public Cylinder()
+		{
+			Radius = 0;
+			Height = 0;
+		}
+		public Cylinder(double Radius, double Height)
+		{
+			this.Radius = Radius;
+			this.Height = Height;
+		}
+		public double radius
+		{
+			get { return this.Radius; }
+			set { this.Radius = value; }
+		}
+		public double height
+		{
+			get { return this.Height; }
+			set { this.Height = value; }
+		}
+
+		public void Nhap()
+		{
+			Console.WriteLine(" Nhập bán kính hình trụ:");
+			Radius = double.Parse(Console.ReadLine());
+			Console.WriteLine("Nhập chiều cao hình trụ:");
+			Height = double.Parse(Console.ReadLine());
+		}
+		public void Hienthi()
+		{
+			Console.WriteLine("Cylinder Characteristics");
+			Console.WriteLine("Radius: {0}, Height: {1}", Radius, Height);
+		}
+
+        
+        double BaseArea, LateralArea, TotalArea, Volume;
+        public void Process()
+		{
+			
+            BaseArea = Radius * Radius * Math.PI;
+            LateralArea = 2 * Math.PI * Radius * Height;
+            TotalArea = 2 * Math.PI * Radius * (Height + Radius);
+            Volume = Math.PI * Radius * Radius * Height;
+        }
+		public void Result()
+		{
+			Console.WriteLine("Base: {0}  ; Lateral: {1}  ; Total: {2}  ; Volume: {3}", BaseArea, LateralArea, TotalArea, Volume);
+		}
+    }
+}
+
---
+++ Baithithu_Ngay07/baithithu_exam_ngay07/baithithu_exam_ngay07/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace baithithu_exam_ngay07;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Cylinder hinh1 = new Cylinder();
+        hinh1.Nhap();
+        hinh1.Hienthi();
+        hinh1.Process();
+        hinh1.Result();
+        
+        Cylinder hinh2 = new Cylinder();
+        hinh2.Radius = 43;
+        hinh2.Height = 76;
+        hinh2.Process();
+        hinh2.Result();
+        Console.ReadKey();
+    }
+}
+
